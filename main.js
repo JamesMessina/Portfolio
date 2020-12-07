85c6df16f9f338ac4010308f11f531d7c2b43aaa
James Messina <jmessina17@gmail.com>
2020-12-06 22:38:04 -0600

    final project done

---
+++ main.js
@@ -0,0 +1,15 @@
+
+function Capitalize(){
+    var x = document.getElementById("name");
+    x.value = x.value.toUpperCase();
+}
+
+function Capitalize2(){
+    var x = document.getElementById("email");
+    x.value = x.value.toUpperCase();
+}
+
+function Capitalize3(){
+    var x = document.getElementById("address");
+    x.value = x.value.toUpperCase();
+}
