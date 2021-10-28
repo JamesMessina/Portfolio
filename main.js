2b2c978778f74dfebbfb74948e0ea880f43bef14
James Messina <jmessina17@gmail.com>
2021-10-28 15:14:30 -0500

    almost done with updating

--- main.js
+++ main.js
@@ -13,3 +13,18 @@ function Capitalize3(){
     var x = document.getElementById("address");
     x.value = x.value.toUpperCase();
 }
+
+function submitForm(){
+    const allInputs = document.querySelectorAll('input');
+    const textBox = document.querySelector('textarea');
+    textBox.value = ""; 
+    for(let i = 0; i < allInputs.length; i++){
+        if(allInputs[i].value === "SEND"){
+            continue
+        }else{
+            console.log(allInputs[i].value, allInputs[i].checked);
+            allInputs[i].value = "";
+            allInputs[i].checked = false;  
+        }
+    }
+}
